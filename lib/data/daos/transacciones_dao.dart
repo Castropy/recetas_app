8cf59766767e738eff9534da6eb5654105999c69
Castropy <randrescastroi@gmail.com>
2026-03-12 21:02:36 -0400

    feat(dao): implement history methods in TransaccionesDao

--- lib/data/daos/transacciones_dao.dart
+++ lib/data/daos/transacciones_dao.dart
@@ -6,4 +6,16 @@ part 'transacciones_dao.g.dart';
 @DriftAccessor(tables: [Transacciones])
 class TransaccionesDao extends DatabaseAccessor<AppDatabase> with _$TransaccionesDaoMixin {
   TransaccionesDao(super.db);
+
+  // Inserción de una Transacción
+  Future<int> insertTransaccion(TransaccionesCompanion transaccion) {
+    return into(transacciones).insert(transaccion);
+  }
+
+  // Stream para observar el historial de transacciones ordenado por fecha
+  Stream<List<Transaccione>> watchAllTransacciones() {
+    return (select(transacciones)
+          ..orderBy([(t) => OrderingTerm(expression: t.fechaHora, mode: OrderingMode.desc)]))
+        .watch();
+  }
 }
---
+++ lib/data/daos/transacciones_dao.g.dart
@@ -0,0 +1,8 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'transacciones_dao.dart';
+
+// ignore_for_file: type=lint
+mixin _$TransaccionesDaoMixin on DatabaseAccessor<AppDatabase> {
+  $TransaccionesTable get transacciones => attachedDatabase.transacciones;
+}
