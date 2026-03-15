63672a6292a258e5fe4a002635e4b8ed7096fa67
Castropy <randrescastroi@gmail.com>
2026-03-14 22:28:06 -0400

    refactor: barrel file for receta module

---
+++ lib/screens/Screen_recetas/widgets/widgets.dart
@@ -0,0 +1,6 @@
+export 'costo_total_section.dart';
+export 'detalle_ingredientes_list.dart';
+export 'detalle_receta_header.dart';
+export 'ingrediente_selector.dart';
+export 'lista_ingredientes_seleccionados.dart';
+export 'nombre_receta_field.dart';
