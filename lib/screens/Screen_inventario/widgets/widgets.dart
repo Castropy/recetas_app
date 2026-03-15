442b080adec91dce76c83b7a1a7b4a748ad5f530
Castropy <randrescastroi@gmail.com>
2026-03-14 22:20:47 -0400

    style: implement barrel file for inventory widgets to simplify imports

---
+++ lib/screens/Screen_inventario/widgets/widgets.dart
@@ -0,0 +1,5 @@
+export 'ingrediente_list_view.dart';
+export 'inventario_action_buttons.dart';
+export 'inventario_form_fields.dart';
+export 'inventario_form_container.dart';
+export 'inventario_data_view.dart';
