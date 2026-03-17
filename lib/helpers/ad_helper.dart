1cf9baa31164c215ed6ea5fa04f231c89c5f3635
Castropy <randrescastroi@gmail.com>
2026-03-17 17:03:51 -0400

    feat: configure production AdMob IDs and update AndroidManifest

--- lib/helpers/ad_helper.dart
+++ lib/helpers/ad_helper.dart
@@ -3,24 +3,24 @@ import 'package:flutter/foundation.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class AdHelper {
-  // --- IDs DE BANNER (Ya los tenías) ---
+  // --- IDs DE PRUEBA OFICIALES ---
   static const String _androidBannerTestId = 'ca-app-pub-3940256099942544/6300978111';
   static const String _iosBannerTestId = 'ca-app-pub-3940256099942544/2934735716';
-
-  // --- IDs DE INTERSTICIAL (PRUEBA) ---
   static const String _androidInterstitialTestId = 'ca-app-pub-3940256099942544/1033173712';
   static const String _iosInterstitialTestId = 'ca-app-pub-3940256099942544/4411468910';
 
-  // TODO: Sustituir por IDs REALES antes de subir a la Play Store
-  static const String _androidInterstitialRealId = 'ca-app-pub-XXXXXXXXXXXXXXXX/XXXXXXXXXX';
-  
+  // --- TUS IDs REALES (RECETAS APP) ---
+  static const String _androidBannerRealId = 'ca-app-pub-6576213341321650/7948394580';
+  static const String _androidInterstitialRealId = 'ca-app-pub-6576213341321650/5435011965';
 
+  // Getter para el Banner
   static String get bannerAdUnitId => kReleaseMode 
-    ? (Platform.isAndroid ? 'TU_ID_REAL_BANNER' : 'TU_ID_REAL_IOS_BANNER') 
+    ? (Platform.isAndroid ? _androidBannerRealId : '') // Dejar vacío si no tienes ID de iOS aún
     : (Platform.isAndroid ? _androidBannerTestId : _iosBannerTestId);
 
+  // Getter para el Intersticial
   static String get interstitialAdUnitId => kReleaseMode
-    ? (Platform.isAndroid ? _androidInterstitialRealId : 'TU_ID_REAL_IOS_INT')
+    ? (Platform.isAndroid ? _androidInterstitialRealId : '') 
     : (Platform.isAndroid ? _androidInterstitialTestId : _iosInterstitialTestId);
 
   /// Carga y muestra un anuncio a pantalla completa de forma inmediata
@@ -30,10 +30,8 @@ class AdHelper {
       request: const AdRequest(),
       adLoadCallback: InterstitialAdLoadCallback(
         onAdLoaded: (ad) {
-          // Tan pronto como carga, lo mostramos
           ad.show();
           
-          // Limpiamos la memoria cuando se cierra o falla
           ad.fullScreenContentCallback = FullScreenContentCallback(
             onAdDismissedFullScreenContent: (ad) => ad.dispose(),
             onAdFailedToShowFullScreenContent: (ad, error) => ad.dispose(),
